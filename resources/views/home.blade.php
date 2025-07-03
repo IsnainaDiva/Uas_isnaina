c501b320aed21fb09f2c65e06e3d65409b8d3eed
IsnainaDiva <isnainadivalbs@gmail.com>
2025-07-03 10:06:55 +0700

    bootstrap

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vehicles|Home</title>
+  </head>
+  <body>
+    <h1>Vehicles</h1>
+  </body>
+</html>
